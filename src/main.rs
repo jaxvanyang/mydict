db4aced26ff78f75c32e6ac963a029a0558f380a
Jax Young <jaxvanyang@gmail.com>
2025-04-08 11:27:58 +0800

    feat: candidate word list

--- src/main.rs
+++ src/main.rs
@@ -2,82 +2,38 @@ use std::{fmt::Write, fs};
 
 use directories::ProjectDirs;
 use iced::{
-	Element, Theme,
+	Element,
+	Length::Fill,
+	Subscription, Task, Theme,
 	widget::{
 		Column, Row, column, container, horizontal_rule, markdown, row, scrollable, text,
 		text_input, vertical_rule,
 	},
+	window,
 };
 use odict::{DefinitionType, Dictionary, DictionaryReader, Entry};
 
-pub const PROJECT_NAME: &str = "mydict";
-pub const PROJECT_TITLE: &str = "My Dictionary";
+pub const NAME: &str = "mydict";
+pub const TITLE: &str = "My Dictionary";
 
 #[derive(Debug)]
 struct State {
 	pub dictionaries: Vec<Dictionary>,
 	pub search_word: String,
 	pub word_list: Vec<String>,
-	pub dict_list: Vec<String>,
 	pub word_description: Vec<markdown::Item>,
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
+	Created,
 	SearchChanged(String),
 	LinkClicked(markdown::Url),
 }
 
-impl State {
-	pub fn view(&self) -> Element<Message> {
-		row![
-			container(scrollable(Column::from_iter(
-				self.word_list
-					.iter()
-					.map(|s| container(text!("{s}")).padding(5).into())
-			)),)
-			.width(200)
-			.padding(5),
-			vertical_rule(2),
-			column![
-				container(text_input("", &self.search_word).on_input(Message::SearchChanged),)
-					.height(50)
-					.padding(10),
-				horizontal_rule(2),
-				container(scrollable(Row::from_iter(
-					self.dict_list
-						.iter()
-						.map(|s| container(text!("{s}")).padding(5).into())
-				)))
-				.padding(5),
-				horizontal_rule(2),
-				container(scrollable(
-					markdown::view(&self.word_description, Theme::default())
-						.map(Message::LinkClicked),
-				))
-				.padding(10),
-			],
-		]
-		.into()
-	}
-
-	pub fn update(&mut self, message: Message) {
-		if let Message::SearchChanged(s) = message {
-			self.search_word = s.clone();
-			if !self.dictionaries[0].entries.contains_key(&s) {
-				self.word_description.clear();
-				return;
-			}
-
-			let entry = &self.dictionaries[0].entries[&s];
-			self.word_description = markdown::parse(&entry2md(entry)).collect();
-		}
-	}
-}
-
 impl Default for State {
 	fn default() -> Self {
-		let proj_dirs = ProjectDirs::from("", "", PROJECT_NAME).unwrap();
+		let proj_dirs = ProjectDirs::from("", "", NAME).unwrap();
 		let data_dir = proj_dirs.data_dir();
 		if !data_dir.exists() {
 			fs::create_dir(data_dir).expect("created directory");
@@ -103,10 +59,6 @@ impl Default for State {
 				}
 			})
 			.collect::<Vec<Dictionary>>();
-		let word_list = dictionaries[0].lexicon()[..1000]
-			.iter()
-			.map(|s| s.to_string())
-			.collect::<Vec<String>>();
 		let word_md = entry2md(&dictionaries[0].entries["do"]);
 		let word_description = markdown::parse(&word_md).collect::<Vec<markdown::Item>>();
 
@@ -115,16 +67,102 @@ impl Default for State {
 		Self {
 			dictionaries,
 			search_word: String::default(),
-			word_list,
-			dict_list: vec!["Open English WordNet".to_string(), "OPTED".to_string()],
+			word_list: Vec::new(),
 			word_description,
+		}
+	}
+}
+
+impl State {
+	pub fn view(&self) -> Element<Message> {
+		let ui = row![
+			container(scrollable(Column::from_iter(
+				self.word_list
+					.iter()
+					.map(|s| container(text!("{s}")).padding(5).into())
+			)),)
+			.width(200)
+			.padding(5),
+			vertical_rule(2),
+			column![
+				container(
+					text_input("", &self.search_word)
+						.id("search")
+						.on_input(Message::SearchChanged),
+				)
+				.height(50)
+				.padding(10),
+				horizontal_rule(2),
+				container(scrollable(Row::from_iter(self.dictionaries.iter().map(
+					|d| {
+						container(text!(
+							"{}",
+							d.name.as_ref().expect("dictionary should have name")
+						))
+						.padding(5)
+						.into()
+					}
+				))))
+				.padding(5),
+				horizontal_rule(2),
+				scrollable(
+					container(
+						markdown::view(&self.word_description, Theme::default())
+							.map(Message::LinkClicked),
+					)
+					.width(Fill)
+					.padding(10)
+				),
+			],
+		];
+
+		ui.into()
+	}
+
+	pub fn update(&mut self, message: Message) -> Task<Message> {
+		match message {
+			Message::Created => text_input::focus("search"),
+			Message::SearchChanged(s) => {
+				self.search_word = s.clone();
+				let s = s.trim();
+				let dict = &self.dictionaries[0];
+				self.word_list = dict
+					.lexicon()
+					.into_iter()
+					.filter_map(|t| {
+						if t.starts_with(s) {
+							Some(t.to_string())
+						} else {
+							None
+						}
+					})
+					.take(1000)
+					.collect();
+
+				if !self.dictionaries[0].entries.contains_key(s) {
+					self.word_description.clear();
+					return Task::none();
+				}
+
+				let entry = &dict.entries[s];
+				self.word_description = markdown::parse(&entry2md(entry)).collect();
+
+				Task::none()
+			}
+			_ => Task::none(),
 		}
 	}
 }
 
 fn main() -> iced::Result {
 	eprintln!("Initialization starting...");
-	iced::run(PROJECT_TITLE, State::update, State::view)
+	iced::application(TITLE, State::update, State::view)
+		.subscription(subscription)
+		.run()
+}
+
+fn subscription(_: &State) -> Subscription<Message> {
+	window::open_events().map(|_| Message::Created)
 }
 
 /// Parse ODict Entry to markdown String
